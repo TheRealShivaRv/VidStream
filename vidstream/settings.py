72d7f4fa41d6d3693cd71e0b634806c7bd5be0b1
TheRealShivaRv <sanders.yorkshire.150b@gmail.com>
2021-12-01 18:40:33 +0530

    Updated to Python 3.10

--- vidstream/settings.py
+++ vidstream/settings.py
@@ -133,7 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.abc.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'abc@abc.com'
 EMAIL_HOST_PASSWORD = '1111'
